48594952f243a0ec84b595f716aa8c45303c42e9
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-06-23 03:31:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
-   def drake_made_this
+ def drake_made_this
     self.artist = Artist.find_or_create_by(name: "Drake")
-  end 
+end 
   def get_genre_name
     self.genre.name
   end	  
